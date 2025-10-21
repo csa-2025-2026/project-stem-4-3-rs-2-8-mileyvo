f9f6b97170d3c1696e79837e65aeb71b43b55660
mileyvo <mtvo106@student.hbuhsd.edu>
2025-10-21 18:21:39 +0000

    10/21/2025

--- Main.java
+++ Main.java
@@ -5,5 +5,20 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+    //Problem 1
+    for (int count = 1; count <= 25; count += 2)
+    {
+      System.out.println(count);
+    }
+
+    // Problem 2
+    for (int count = 17; count <= 73; count++)
+    {
+      System.out.print(count + " ");
+      if (count % 11 == 0)
+      {
+        System.out.println();
+      }
+    }
   }
 }
